5e98cc9686e8198ae1cecc6f2702c1ab5ebf879f
wang-z-j <794401136@qq.com>
2020-04-07 21:13:41 +0800

    项目结构搭建

--- mimall/src/App.vue
+++ mimall/src/App.vue
@@ -1,21 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 <script>
 // import axios from 'axios'
 export default {
-
-  data () {
-    return {
-      num: 1
-    }
-  },
   mounted () {
   },
   methods: {
